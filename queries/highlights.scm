851280eecd21f941546333f2bdb01d3c41018d7c
Farhan Khan <comptergeeks@gmail.com>
2025-04-08 17:46:25 -0700

    fixing highlights

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -1,30 +1,14 @@
 ;; Keywords
-[
-  "dynamic"
-  "function"
-  "test"
-  "client"
-  "generator"
-  "retry_policy"
-  "type_builder"
-  "template_string"
-  "string_template"
-  "class"
-  "enum"
-  "type"
-  "map"
-  "fn"
-  "let"
-] @keyword
+((identifier) @keyword
+ (#match? @keyword "^(function|test|client|generator|retry_policy|type_builder|class|enum|type|map|fn|let|dynamic)$"))
 
-;; Identifier paths
-(path_identifier) @variable
-(namespaced_identifier) @namespace
+;; Identifiers
+(identifier) @variable
 
 ;; String literals
 (quoted_string_literal) @string
 (raw_string_literal) @string
-(unquoted_string_literal) @string.special
+(unquoted_string_literal) @string
 
 ;; Numeric literals
 (numeric_literal) @number
@@ -35,69 +19,23 @@
 (block_comment) @comment
 
 ;; Types
-(field_type (identifier) @type)
-(array_notation) @type
-(map_type) @type
-(literal_type) @type
-(field_type_with_attr) @type
+(field_type) @type
+
+;; Functions
+(expr_fn name: (identifier) @function)
+(value_expression_block name: (identifier) @function)
+
+;; Properties
+(value_expression name: (identifier) @property)
+(type_expression name: (identifier) @property)
+
+;; Parameters
+(named_argument name: (identifier) @variable.parameter)
+
+;; Punctuation
+["(" ")" "[" "]" "{" "}" "<" ">" "," ";" ":"] @punctuation.bracket
+["|" "=" "?" "=>" "->"] @operator
 
 ;; Attributes
 (field_attribute) @attribute
 (block_attribute) @attribute
-
-;; Operators
-[
-  "|"
-  "="
-  "?"
-  "=>"
-  "->"
-  "::"
-  "."
-] @operator
-
-;; Punctuation
-[
-  "("
-  ")"
-  "["
-  "]"
-  "{"
-  "}"
-  "<"
-  ">"
-  ","
-  ";"
-  ":"
-  "@"
-  "@@"
-] @punctuation.bracket
-
-;; Expression blocks
-(expr_block) @expression
-
-;; Functions
-(expr_fn name: (identifier) @function)
-(value_expression_block
-  keyword: (value_expression_keyword) @keyword
-  name: (identifier) @function)
-
-;; Properties and fields
-(value_expression name: (identifier) @property)
-(type_expression name: (identifier) @property)
-(class_field_value_pair field_name: (identifier) @property)
-
-;; Template declarations
-(template_declaration name: (identifier) @function)
-
-;; Function applications
-(fn_app function_name: (identifier) @function)
-
-;; Named arguments
-(named_argument name: (identifier) @variable.parameter)
-
-;; Map expressions
-(map_key) @property
-
-;; Jinja expressions
-(jinja_expression) @embedded
