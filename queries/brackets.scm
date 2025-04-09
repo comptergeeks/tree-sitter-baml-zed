a3c3058d61c99b451adfe54d6840d88114eec076
Farhan Khan <comptergeeks@gmail.com>
2025-04-08 17:40:41 -0700

    fixing brackets

--- queries/brackets.scm
+++ queries/brackets.scm
@@ -1,103 +1,14 @@
-;; Keywords
-[
-  "dynamic"
-  "function"
-  "test"
-  "client"
-  "generator"
-  "retry_policy"
-  "type_builder"
-  "template_string"
-  "string_template"
-  "class"
-  "enum"
-  "type"
-  "map"
-  "fn"
-  "let"
-] @keyword
+;; Standard brackets
+("(" @open ")" @close)
+("[" @open "]" @close)
+("{" @open "}" @close)
+("<" @open ">" @close)
 
-;; Identifier paths
-(path_identifier) @variable
-(namespaced_identifier) @namespace
+;; String quotes
+("\"" @open "\"" @close)
 
-;; String literals
-(quoted_string_literal) @string
-(raw_string_literal) @string
-(unquoted_string_literal) @string.special
+;; Jinja expression delimiters
+("{{" @open "}}" @close)
 
-;; Numeric literals
-(numeric_literal) @number
-
-;; Comments
-(comment) @comment
-(doc_comment) @comment.doc
-(block_comment) @comment
-
-;; Types
-(field_type (identifier) @type)
-(array_notation) @type
-(map_type) @type
-(literal_type) @type
-(field_type_with_attr) @type
-
-;; Attributes
-(field_attribute) @attribute
-(block_attribute) @attribute
-
-;; Operators
-[
-  "|"
-  "="
-  "?"
-  "=>"
-  "->"
-  "::"
-  "."
-] @operator
-
-;; Punctuation
-[
-  "("
-  ")"
-  "["
-  "]"
-  "{"
-  "}"
-  "<"
-  ">"
-  ","
-  ";"
-  ":"
-  "@"
-  "@@"
-] @punctuation.bracket
-
-;; Expression blocks
-(expr_block) @expression
-
-;; Functions
-(expr_fn name: (identifier) @function)
-(value_expression_block
-  keyword: (value_expression_keyword) @keyword
-  name: (identifier) @function)
-
-;; Properties and fields
-(value_expression name: (identifier) @property)
-(type_expression name: (identifier) @property)
-(class_field_value_pair field_name: (identifier) @property)
-
-;; Template declarations
-(template_declaration name: (identifier) @function)
-
-;; Function applications
-(fn_app function_name: (identifier) @function)
-
-;; Named arguments
-(named_argument name: (identifier) @variable.parameter)
-
-;; Map expressions
-(map_key) @property
-
-;; Jinja expressions
-(jinja_expression) @embedded
+;; Block comment delimiters
+("{//" @open "//}" @close)
